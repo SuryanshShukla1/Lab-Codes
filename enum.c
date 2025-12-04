54e661297d8c19ba87cedc6631e5937717545c8c
Suryansho.0 <suryansh44555@gmail.com>
2025-12-04 12:15:47 +0530

    Create enum.c

---
+++ enum.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// Define an enum for days of the week
+enum Day {
+    SUNDAY,    // 0
+    MONDAY,    // 1
+    TUESDAY,   // 2
+    WEDNESDAY, // 3
+    THURSDAY,  // 4
+    FRIDAY,    // 5
+    SATURDAY   // 6
+};
+
+int main() {
+    // Declare a variable of enum type
+    enum Day today;
+    
+    // Assign a value
+    today = WEDNESDAY;
+    
+    printf("Today is day number: %d\n", today);
+    
+    // Use enum in a switch statement
+    switch(today) {
+        case SUNDAY:
+            printf("It's Sunday - Weekend!\n");
+            break;
+        case MONDAY:
+            printf("It's Monday - Start of work week\n");
+            break;
+        case WEDNESDAY:
+            printf("It's Wednesday - Midweek\n");
+            break;
+        case FRIDAY:
+            printf("It's Friday - Almost weekend!\n");
+            break;
+        case SATURDAY:
+            printf("It's Saturday - Weekend!\n");
+            break;
+        default:
+            printf("It's another weekday\n");
+    }
+    
+    return 0;
+}
